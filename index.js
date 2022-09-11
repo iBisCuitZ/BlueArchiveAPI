439bf7c0f379513940e8e723e66d0d048f0f80fc
iBisCuitZ <toontown_zero@live.com>
2022-09-11 10:15:47 +0700

    BA API
    can request by /students or /students/:id

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const student = require('./resource/data/en/students.json')
+const gear = require('./resource/data/en/equipment.json')
+
+app.use(express.json());
+
+app.get("/students",(req,res)=>{
+    res.send(student[1])
+})
+
+app.get("/students/:id",(req,res)=>{
+    const {id} = req.params;
+    try{
+        console.log(id+" "+student.length)
+        if (id > student.length){
+            throw new Error();
+        }
+        res.send(student[id-1])
+    } catch {
+        console.log(id+" "+student.length);
+        res.send(`Student ID can only be number and between 1-${student.length}`);
+    }
+})
+
+
+
+app.listen(3000,()=>{
+    console.log('Node.JS on port 3000');
+})
+
