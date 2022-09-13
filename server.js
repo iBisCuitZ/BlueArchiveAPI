3ca843d44dd6867622767da0cf947a2ee49e2d9b
iBisCuitZ <toontown_zero@live.com>
2022-09-13 08:45:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const student = require('./resource/data/en/students.json')
 const gear = require('./resource/data/en/equipment.json')
 app.use(express.static( __dirname + '/' ));
 app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("use /student to get student List")
+})
 app.get("/students",(req,res)=>{
     res.send(student)
 })
-
 app.get("/students/:id",(req,res)=>{
     const {id} = req.params;
     try{
@@ -23,8 +26,26 @@ app.get("/students/:id",(req,res)=>{
     }
 })
 
+<<<<<<< Updated upstream:server.js
 app.get("/*",(req,res)=>{
     res.sendfile('error.html')
+=======
+app.get("/gears",(req,res)=>{
+    res.send(gear)
+})
+
+app.get("/gears/:id",(req,res)=>{
+    const {id} = req.params;
+    try{
+        if (id > gear.length){
+            throw new Error();
+        }
+        res.send(gear[id])
+    } catch {
+        console.log(id+" "+gear.length);
+        res.send(`Equipment ID can only be number and between 1-${gear.length-1}`);
+    }
+>>>>>>> Stashed changes:index.js
 })
 
 
